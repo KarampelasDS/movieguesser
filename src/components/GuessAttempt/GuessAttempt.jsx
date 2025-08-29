ee9fa89acb9055a663b81ac99230ff9251abf00e
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2025-08-29 15:32:08 +0300

    Removed Unused Components

--- src/components/GuessAttempt/GuessAttempt.jsx
+++ src/components/GuessAttempt/GuessAttempt.jsx
@@ -1,9 +1,9 @@
 import { IoCheckmark } from "react-icons/io5";
 import { RxCross1 } from "react-icons/rx";
 
-export default function GuessAttempt(props){
-    return(
-        <div className="GuessAttempt">
+export default function GuessAttempt(props) {
+  return (
+    <div className="GuessAttempt">
       <div className="search-results">
         <div className="search-results-image">
           <img
@@ -19,10 +19,17 @@ export default function GuessAttempt(props){
           {props.title}
           <br />({props.year ? props.year.slice(0, 4) : "N/A"})
         </div>
-        <div className="GuessResult" style={props.correct ? {backgroundColor: "#1AB16F"} : {backgroundColor: "#DF2723"}}>
-            {props.correct ? <IoCheckmark size={90}/> : <RxCross1 size={90}/>}
+        <div
+          className="GuessResult"
+          style={
+            props.correct
+              ? { backgroundColor: "#1AB16F" }
+              : { backgroundColor: "#DF2723" }
+          }
+        >
+          {props.correct ? <IoCheckmark size={90} /> : <RxCross1 size={90} />}
         </div>
       </div>
     </div>
-    );
+  );
 }
---
+++ src/components/Results/Results.jsx
@@ -0,0 +1,5 @@
+import useGameManager from "@/store/useGameManager";
+
+export default function Results() {
+  return <div>Results</div>;
+}
